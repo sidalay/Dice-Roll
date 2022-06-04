df906327c7d3894ca75313660d282df2b5ad25d4
Sid Shingaki <sidalaytv@gmail.com>
2022-06-04 13:34:51 -0700

    chore: add starting conditions

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const Score0Ele = document.querySelector('#score--0');
 const Score1Ele = document.getElementById('score--1');
+const DiceEle = document.querySelector('.dice');
 
+// Starting conditions
 Score0Ele.textContent = 0;
 Score1Ele.textContent = 0;
+DiceEle.classList.add('hidden');
