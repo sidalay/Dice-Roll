dac52a784b2d60588bc67cf214217094928cacfd
Sid Shingaki <sidalaytv@gmail.com>
2022-06-04 13:33:25 -0700

    chore: grabbing score elements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// Selecting elements
+const Score0Ele = document.querySelector('#score--0');
+const Score1Ele = document.getElementById('score--1');
+
+Score0Ele.textContent = 0;
+Score1Ele.textContent = 0;
